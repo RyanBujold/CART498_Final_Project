104f2ac6417e4f7ccd81cae242635b67b5db53f7
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2026-03-21 16:06:48 -0400

    feat: displaying a test page and beginning the js script

---
+++ static/script.js
@@ -0,0 +1,26 @@
+function displayMaze() {
+    document.getElementById("maze").innerHTML=`
+    <pre>
+    #####
+    #E  #
+    ### #
+    #X  #
+    #####
+    </pre>`;
+}
+
+function moveRight() {
+
+}
+
+function moveLeft(){
+
+}
+
+function moveUp(){
+
+}
+
+function moveDown(){
+    
+}
